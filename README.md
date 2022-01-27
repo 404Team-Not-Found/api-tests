33ee7ddbcb5bd4f86d669a3f0c75e1e8af15405d
JarJarBinks <Admin@jar-jar-binks.local>
2022-01-27 11:07:59 -0700

    added config and test

--- README.md
+++ README.md
@@ -2,4 +2,3 @@
     API tests for DevOps
     Team Devops
 
-    contributing, ig. sorry i was absent for so long
---
+++ test/test.js
@@ -0,0 +1,34 @@
+import 'assert'; from ('assert')
+
+const fetch = require("node-fetch");
+const config = require("config");
+
+const loginUrl = config.get("pitch59-url") + "/api/account/login";
+
+it(`Testing to see if we can signin`, async () => {
+  let options = {
+    method: "POST",
+    headers: {
+      "content-type": "application/json"
+    },
+    body: JSON.stringify({
+      emailId: "highground@jedicouncil.com",
+      password: "High!ground69"
+    })
+  };
+
+  let errorWasCaught = false;
+  let errorCaught = null;
+  let json = null;
+  try {
+    const response = await fetch(loginUrl, options);
+    json = await response.json();
+    console.log("Response", json);
+  } catch (exception) {
+    errorCaught = exception;
+    errorWasCaught = true;
+    throw console.log(errorCaught);
+  }
+
+  assert.equal(errorWasCaught,false);
+});
